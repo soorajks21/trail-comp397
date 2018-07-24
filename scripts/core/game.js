fd5ed4f1f0ec05d54c1a7956c0e45fc69331762e
Sooraj Suresh <ssuresh9@my.centennialcollege.ca>
2018-07-23 22:56:35 -0400

    added npm support

---
+++ scripts/core/game.js
@@ -0,0 +1,23 @@
+//IIFA 
+(function(){
+
+function init(){
+
+    console.log("Started the intilaization");
+}
+
+function start(){
+
+}
+
+function Update(){
+
+}
+
+function Main(){
+
+
+}
+
+window.onload = init;
+})();
